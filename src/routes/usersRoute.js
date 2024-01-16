def5512621cbc4756824299efa93661557f73d42
Serkan Sönmez <serkansonmzz@gmail.com>
2024-01-16 16:17:51 +0300

    userRoute was created and get routings were defined in it

---
+++ src/routes/usersRoute.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import short from "short-uuid"; //short.generate();
+import users from "../data/users.json";
+const router = Router();
+
+router.get("/", (res, req) => {
+  res.send("Welcome To Home Page");
+});
+
+router.get("/users", (req, res) => {
+  if (users.length === 0) {
+    return res.status(404).json({ message: "Users not found" });
+  }
+  res.json(users);
+});
